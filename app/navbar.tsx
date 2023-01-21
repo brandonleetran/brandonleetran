a6b346cd8945f8b4299e629a5b726980b04dab0c
Brandon Lee Tran <brandonleetran@icloud.com>
2023-01-20 21:29:53 -0600

    feat: added navigation component

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -1,7 +1,33 @@
-export default function Navigation() {
-    return(
-        <header>
-            This is from the Navigation component
-        </header>
-    )
-}
+"use client"
+import * as NavigationMenu from '@radix-ui/react-navigation-menu';
+
+export default () => (
+  <NavigationMenu.Root>
+    <NavigationMenu.List>
+      <NavigationMenu.Item>
+        <NavigationMenu.Trigger />
+        <NavigationMenu.Content>
+          <NavigationMenu.Link />
+        </NavigationMenu.Content>
+      </NavigationMenu.Item>
+
+      <NavigationMenu.Item>
+        <NavigationMenu.Link />
+      </NavigationMenu.Item>
+
+      <NavigationMenu.Item>
+        <NavigationMenu.Trigger />
+        <NavigationMenu.Content>
+          <NavigationMenu.Sub>
+            <NavigationMenu.List />
+            <NavigationMenu.Viewport />
+          </NavigationMenu.Sub>
+        </NavigationMenu.Content>
+      </NavigationMenu.Item>
+
+      <NavigationMenu.Indicator />
+    </NavigationMenu.List>
+
+    <NavigationMenu.Viewport />
+  </NavigationMenu.Root>
+);
