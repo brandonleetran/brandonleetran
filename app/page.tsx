679e378727f98cfb8ab302901c17750acc87bae7
Brandon Lee Tran <brandonleetran@icloud.com>
2023-02-02 18:09:09 -0600

    fix: removed unused styles

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Page() {
     <section className="prose prose-zinc prose-sm md:prose-md">
       <div className="flex flex-col-reverse md:flex-row pb-10 mb-10 bg-gradient-to-r from-rose-400 via-fuchsia-500 from-indigo-500 bg-[length:100%_2px] bg-no-repeat bg-bottom">
         <div className="basis-2/3 max-w-md">
-          <h1 className="m-0 font-bold">Brandon Lee Tran</h1>
+          <h1 className="m-0">Brandon Lee Tran</h1>
           <p className="mt-1">Software Developer</p>
           <p className="mb-0">Welcome, I am a Front-end Developer based in Omaha, NE with a passion for full-stack development. I enjoy learning new technologies, cooking, and gaming on the side. This site is currently under development. Please stay tuned for more ongoing changes.</p>
         </div>
