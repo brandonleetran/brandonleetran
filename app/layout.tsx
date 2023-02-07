18ed95e6ea1b9ab135854fe5a9131dd2cec93224
brandonleetran <brandonleetran@icloud.com>
2023-02-07 00:59:02 -0600

    feat: social media links

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="text-3x h-screen overflow-auto">
-      <body className="text-black bg-white h-screen w-full overflow-auto">
+      <body className="text-black bg-white h-screen w-full overflow-auto pb-20">
         <Navigation/>
         <main className="prose-slate md:mt-5 p-5 md:pl-4 md:p-0 max-w-2xl m-auto">
           {children}
--- app/page.tsx
+++ app/page.tsx
@@ -19,6 +19,58 @@ export default function Page() {
       <p className="text-black">I <b>enjoy</b> building websites, especially in front-end development. I am an academic learner, problem solver, and critical thinker. My Top 5 CliftonStrengths are <i>Achiever, Restorative, Deliberative, Learner, & Analytical</i>.</p>
       <p className="text-black">Outside of software, I am <b>passionate</b> about many pursuits. These include health/fitness, cooking, playing video games, sports, and listening to new music. My <b>favorite</b> food to cook is steak, my favorite game to play is Valorant, and my favorite teams are the Miami Heat and Minnesota Vikings.</p>
       <p className="text-black">This site is currently hosted on Vercel as it is built with <b>NextJS</b> with a <i>sprinkle</i> of <b>Tailwind</b>. You can check out my portfolio, read my blogs, or insert a drop. You can follow me on socials listed below. Thanks for reading.</p>
+      <div className="flex justify-center flex-col mt-10 gap-5">
+        <a href="https://github.com/brandonleetran" target="_blank" className="w-full h-full border hover:border-white hover:bg-gradient-to-r bg-white transition-all from-rose-400 via-fuchsia-500 from-indigo-500 no-underline rounded-lg hover:p-[3px]">
+          <div className="transition-all p-5 w-full flex flex-row justify-between rounded-md bg-white">
+            <div className="flex">
+              <div className="m-auto">
+                <Logo path="/logos/github.png" />
+              </div>
+              <div className="ml-5 flex flex-col">
+                <div className="font-bold">@brandonleetran</div>
+                <small className="text-stone-500">Github</small>
+              </div>
+            </div>
+            <div className="my-auto">
+              <Image width="15" height="15" className="m-0 ring-gray-300" src="/logos/external-link-symbol.png" alt="Bordered avatar" />
+            </div>
+          </div>
+        </a>
+
+        <a href="https://www.linkedin.com/in/brandon-lee-tran" target="_blank" className="w-full h-full border hover:border-white hover:bg-gradient-to-r bg-white transition-all from-rose-400 via-fuchsia-500 from-indigo-500 no-underline rounded-lg hover:p-[3px]">
+          <div className="transition-all p-5 w-full flex flex-row justify-between rounded-md bg-white">
+            <div className="flex">
+              <div className="m-auto">
+                <Logo path="/logos/linkedin.png" />
+              </div>
+              <div className="ml-5 flex flex-col">
+                <div className="font-bold">@brandonleetran</div>
+                <small className="text-stone-500">LinkedIn</small>
+              </div>
+            </div>
+            <div className="my-auto">
+              <Image width="15" height="15" className="m-0 ring-gray-300" src="/logos/external-link-symbol.png" alt="Bordered avatar" />
+            </div>
+          </div>
+        </a>
+
+        <a href="https://twitter.com/brandonleetrann" target="_blank" className="w-full h-full border hover:border-white hover:bg-gradient-to-r bg-white transition-all from-rose-400 via-fuchsia-500 from-indigo-500 no-underline rounded-lg hover:p-[3px]">
+          <div className="transition-all p-5 w-full flex flex-row justify-between rounded-md bg-white">
+            <div className="flex">
+              <div className="m-auto">
+                <Logo path="/logos/twitter.png" />
+              </div>
+              <div className="ml-5 flex flex-col">
+                <div className="font-bold">@brandonleetran</div>
+                <small className="text-stone-500">Twitter</small>
+              </div>
+            </div>
+            <div className="my-auto">
+              <Image width="15" height="15" className="m-0 ring-gray-300" src="/logos/external-link-symbol.png" alt="Bordered avatar" />
+            </div>
+          </div>
+        </a>
+      </div>
     </section>
   );
 }
