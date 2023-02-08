0eb0217bdb81e76db71118709fb183a970300247
brandonleetran <brandonleetran@icloud.com>
2023-02-07 20:44:51 -0600

    feat: asked mask-icon for safari

--- app/head.tsx
+++ app/head.tsx
@@ -9,6 +9,7 @@ export default async function Head() {
       <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
       <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
       <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+      <link rel="mask-icon" href="/favicon/apple-touch-icon.png" color="#5bbad5"/>
       <link rel="manifest" href="/favicon/site.webmanifest" />
     </>
   );
