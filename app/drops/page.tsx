82dac30dc67144e271379fbab6be043301750c1b
brandonleetran <brandonleetran@icloud.com>
2023-02-14 01:56:14 -0600

    fix: fixing rendering server-side

--- app/drops/page.tsx
+++ app/drops/page.tsx
@@ -3,11 +3,11 @@ import authOptions from "pages/api/auth/[...nextauth]"
 import Login from "components/Login"
 
 export default async function Page() {
-   // this is an example how to get the session from a server component
-  const session = await getServerSession(authOptions)
-  console.log(session)
+  // this is an example how to get the session from a server component
+  // const session = await getServerSession(authOptions)
+  //console.log(session)
 
-  if (!session) {
+  //if (!session) {
     return <Login />
-  }
+  //}
 }
