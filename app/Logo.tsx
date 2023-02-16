a0da56175cd32d3066391ec3fdeebd9c115cc06f
Brandon Lee Tran <45769389+brandonleetran@users.noreply.github.com>
2023-02-15 20:10:07 -0600

    fix: deleting Logo.tsx

--- app/Logo.tsx
+++
@@ -1,9 +0,0 @@
-import Image from 'next/image'
-
-const Logo = ({ path }: { path: string }) => {
-    return (
-        <Image width="25" height="25" className="m-0 ring-gray-300" src={path} alt="Bordered avatar" />
-    )
-}
-
-export default Logo
