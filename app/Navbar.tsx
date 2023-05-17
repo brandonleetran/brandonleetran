ea866ddaddd3fb17bf4aa092816d7ef0ee567fc0
brandonleetran <brandonleetran@icloud.com>
2023-05-17 02:04:58 -0500

    fix: delete sticky nav

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 const Navigation = () => {
   return (
-    <nav className="backdrop-blur-3xl bg-white/[.6] w-full z-10 top-0 sticky md:py-5 pt-3 pb-3" >
+    <nav className="backdrop-blur-3xl bg-white/[.6] w-full z-10 top-0 md:py-5 pt-3 pb-3" >
       <div className="w-full m-auto max-w-2xl container flex flex-wrap items-center justify-end md:justify-center">
         <div className="w-full flex-grow flex items-center bg-transparent text-white z-20" id="nav-content">
           <ul className="text-sm font-normal flex flex-1 items-center text-left pl-1 md:pl-0">
