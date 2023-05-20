85710d4d6e41d0d2e25664a516cccb63d38110e1
Brandon Lee Tran <brandonleetran@icloud.com>
2023-05-20 18:03:39 -0500

    fix: fix manifest error

--- app/drops/page.tsx
+++ app/drops/page.tsx
@@ -6,15 +6,15 @@ import Message from "../../components/Message"
 export default async function Page() {
   // this is an example how to get the session from a server component
   // const session = await getServerSession(authOptions)
-  //console.log(session)
+  // console.log(session)
 
   return (
     <>
         <h1 className="m-0 text-2xl mb-5">#drops</h1>
         <Login/>
         <div className="flex flex-col gap-2 text-sm">
-          <Message avatar="" username="brandonleetran">This is currently in development</Message>
-          <Message avatar="" username="brandonleetran">This is currently in development</Message>
+          <Message avatar="" username="test_user1">This is currently in development</Message>
+          <Message avatar="" username="test_user2">This is currently in development</Message>
         </div>
     </>
   )
