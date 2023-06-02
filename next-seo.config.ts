49f95b76cdfd9ea0ee17e0ee950406242fae4d95
brandonleetran <brandonleetran@icloud.com>
2023-06-02 13:28:34 -0500

    feat: favicon emoji update

--- next-seo.config.ts
+++ next-seo.config.ts
@@ -2,10 +2,10 @@ import type { NextSeoProps } from 'next-seo'
 
 export const NEXT_SEO_DEFAULT: NextSeoProps = {
   title: 'Brandon Lee Tran',
-  description: 'Front-end developer, car enthusiast, dog dad.',
+  description: 'Front-end developer, blogger, dog dad.',
   openGraph: {
     type: 'website',
-    url: 'https://brandonleetran.vercel.app/',
+    url: 'https://brandonleetran.tech/',
     title: 'Brandon Lee Tran - Software Developer',
     description: 'Front-end developer, blogger, dog dad.',
     images: [
