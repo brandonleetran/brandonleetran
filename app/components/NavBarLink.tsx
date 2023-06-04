c98e89b34c892fc74eb5a8c36a863259bd4f713e
brandonleetran <brandonleetran@icloud.com>
2023-06-04 01:38:36 -0500

    feat: refactor navlinks :P

--- app/components/NavBarLink.tsx
+++ app/components/NavBarLink.tsx
@@ -1,25 +1,31 @@
-'use client'
-import { usePathname } from 'next/navigation'
-import Link from 'next/link'
+'use client';
+import { usePathname } from 'next/navigation';
+import Link from 'next/link';
+
+type NavLink = {
+  path: string;
+  name: string;
+};
 
 export interface NavBarLinkProps {
-    children : React.ReactNode,
-    path : string
+  navlink: NavLink;
 }
 
-const NavBarLink = ( { children, path } : NavBarLinkProps) => {
-    console.log("The NavBarLink component was rendered in the client!")
-
-    const pathName = usePathname()
-
-    const isActiveLink = (path : string) => {
-      return pathName === path ? "text-black bg-black/[.05]" : "text-stone-500"
-    }
-    return (
-        <Link href={path} className={`lowercase font-bold hover:text-black bg-transparent inline-block py-2 px-4 no-underline rounded-md transition-bg duration-300 ${isActiveLink(path)}`}>
-            {children}
+const NavBarLink = ({ navlink }: NavBarLinkProps) => {
+  console.log("The NavBarLink component was rendered in the client!")
+  const pathName = usePathname()
+  const isActiveLink = (path: string) => {
+    return pathName === path;
+  };
+  return (
+        <Link
+          href={navlink.path}
+          key={navlink.name}
+          className={`${isActiveLink(navlink.path) ? "text-black bg-black/[.05]" : "text-stone-500"} lowercase font-bold hover:text-black bg-transparent inline-block py-2 px-4 no-underline rounded-md transition-bg duration-300`}
+        >
+          {navlink.name}
         </Link>
-    )
-}
+  );
+};
 
-export default NavBarLink
+export default NavBarLink;
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,33 +1,26 @@
 import NavBarLink from "./NavBarLink"
 
 const Navigation = () => {
+
+  const links = [ 
+    {path: '/', name: "Home" },
+    {path: '/portfolio', name: "Portfolio"},
+    {path: '/blog', name: 'Blog' },
+    {path: '/drops', name: 'Drops'}
+  ]
+
   console.log("The Navbar component is rendered in the server!")
   return (
     <nav className="backdrop-blur-3xl bg-white/[.6] w-full z-10 top-0 sticky md:py-5 pt-3 pb-3" aria-label='Navigation'>
       <div className="w-full m-auto max-w-2xl container flex flex-wrap items-center justify-end md:justify-center">
         <div className="w-full flex-grow flex items-center bg-transparent text-white z-20" id="nav-content">
           <ul className="text-sm font-normal flex flex-1 items-center text-left pl-1 md:pl-0">
-            <li>
-              <NavBarLink path="/">
-                Home
-              </NavBarLink>
-            </li>
-            <li>
-              <NavBarLink path="/portfolio">
-                Portfolio
-              </NavBarLink>
-            </li>
-            <li>
-              <NavBarLink path="/blog">
-                Blog
-              </NavBarLink>
-            </li>
-            <li>
-              <NavBarLink path="/drops">
-                Drops
-              </NavBarLink>
-            </li>
-          </ul>
+            {links.map((link) => (
+                <li key={link.path}>
+                  <NavBarLink navlink={link} />
+                </li>
+              ))}
+            </ul>
         </div>
       </div>
     </nav>
