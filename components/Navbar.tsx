b4c52ebc7d18e2a68cf73cd9a9cd4456001c7ae4
brandonleetran <brandonleetran@icloud.com>
2023-06-04 03:13:31 -0500

    chore: testing

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,5 @@
-'use client'
-
-import NavBarLink from "./NavBarLink"
-
+import NavBarLink from './NavBarLink'
+import Link from 'next/link'
 const Navigation = () => {
 
   const links = [ 
@@ -17,11 +15,21 @@ const Navigation = () => {
       <div className="w-full m-auto max-w-2xl container flex flex-wrap items-center justify-end md:justify-center">
         <div className="w-full flex-grow flex items-center bg-transparent text-white z-20" id="nav-content">
           <ul className="text-sm font-normal flex flex-1 items-center text-left pl-1 md:pl-0">
-            {links.map((link) => (
+          {/* {links.map((link) => (
                 <li key={link.path}>
                   <NavBarLink navlink={link} />
                 </li>
-              ))}
+              ))} */}
+            <li>
+              <Link href='/' className="text-black lowercase font-bold hover:text-black bg-transparent inline-block py-2 px-4 no-underline rounded-md transition-bg duration-300">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link href='/portfolio' className="text-black lowercase font-bold hover:text-black bg-transparent inline-block py-2 px-4 no-underline rounded-md transition-bg duration-300">
+                Portfolio
+              </Link>
+            </li>
             </ul>
         </div>
       </div>
