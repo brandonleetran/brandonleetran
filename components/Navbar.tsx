88597d092cf31250d998b6586258410be168162d
brandonleetran <brandonleetran@icloud.com>
2023-06-04 15:17:21 -0500

    feat: client side navigation

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,14 +1,23 @@
-import NavBarLink from './NavBarLink'
+'use client'
+
+import { usePathname } from 'next/navigation'
+import Link from 'next/link'
+
 const Navigation = () => {
 
+  const pathName = usePathname()
   const links = [ 
-    {path: '/', name: "Home" },
-    {path: '/portfolio', name: "Portfolio"},
-    {path: '/blog', name: 'Blog' },
-    {path: '/drops', name: 'Drops'}
+    { path: '/', name: 'Home' },
+    { path: '/portfolio', name: 'Portfolio' },
+    { path: '/blog', name: 'Blog' },
+    { path: '/drops', name: 'Drops' }
   ]
 
-  console.log("The Navbar component is rendered in the server!")
+  const isActive = (path : string) => {
+    return path === pathName
+  }
+
+  console.log("The navigation component is rendered in the client!")
   return (
     <nav className="backdrop-blur-3xl bg-white/[.6] w-full z-10 top-0 sticky md:py-5 pt-3 pb-3" aria-label='Navigation'>
       <div className="w-full m-auto max-w-2xl container flex flex-wrap items-center justify-end md:justify-center">
@@ -16,7 +25,9 @@ const Navigation = () => {
           <ul className="text-sm font-normal flex flex-1 items-center text-left pl-1 md:pl-0">
             {links.map((link) => (
                   <li key={link.path}>
-                    <NavBarLink navlink={link} />
+                    <Link href={link.path} className={`${isActive(link.path) ? "text-black bg-black/[.05]" : "text-stone-500"} lowercase font-bold hover:text-black bg-transparent inline-block py-2 px-4 no-underline rounded-md transition-bg duration-300`}>
+                      {link.name}
+                    </Link>
                   </li>
                 ))}
             </ul>
