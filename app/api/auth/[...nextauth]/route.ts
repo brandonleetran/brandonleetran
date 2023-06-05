1213c3db390e0bb395c2bd24b2dc67ed2cfb5c3a
brandonleetran <brandonleetran@icloud.com>
2023-06-04 20:14:34 -0500

    fix: fix env variable

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -5,7 +5,7 @@ const handler = NextAuth({
   providers: [
     GithubProvider({
         clientId: process.env.GITHUB_CLIENT_ID,
-        clientSecret: process.env.GITHUB_CLIENT_ID
+        clientSecret: process.env.GITHUB_CLIENT_SECRET
     }),
   ]
 })
