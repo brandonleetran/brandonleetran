ecd5bba5a0b091a7c5b846ff63040e35d65bd4b7
brandonleetran <brandonleetran@icloud.com>
2023-06-10 10:47:30 -0500

    feat: testing for a 202

--- app/page.tsx
+++ app/page.tsx
@@ -21,9 +21,9 @@ const fetchStats = async () : Promise<any | null> => {
       // you'll receive a 202 response; a background job is also fired to start compiling these statistics.
       // you should allow the job a short time to complete, and then submit the request again
       // if the job has completed, that request will receive a 200 response with the statistics in the response body
-      await new Promise(res => setTimeout(res, 5000))
       console.log('202 Accepted. Waiting for the response...')
       console.log(res)
+      await new Promise(res => setTimeout(res, 10000))
       return await fetchStats()
     }
 
