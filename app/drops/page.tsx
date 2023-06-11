511bf3918a4c7be5513aaae3f3827bf1c3979a59
brandonleetran <brandonleetran@icloud.com>
2023-06-11 03:51:30 -0500

    fix: refactor fetchDrops

--- app/drops/page.tsx
+++ app/drops/page.tsx
@@ -4,14 +4,11 @@ import AuthContext from '@/libs/AuthContext'
 import Prisma from '@/libs/Prisma'
 
 const fetchDrops = async () => {
-  const res = await Prisma.drop.findMany()
-  return res
+  return await Prisma.drop.findMany()
 }
 
 export default async function Page() {
   const drops = await fetchDrops()
-  console.log(drops)
-
   return (
     <>
         <h1 className="m-0 text-2xl mb-5">Drops</h1>
