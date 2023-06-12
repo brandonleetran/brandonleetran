ff9e8b5e55c8e0a2ddf1459d4408a56da62e188b
brandonleetran <brandonleetran@icloud.com>
2023-06-11 19:25:45 -0500

    feat: add null validation

--- components/Login.tsx
+++ components/Login.tsx
@@ -30,14 +30,19 @@ export const Login = () => {
     }
 
     // add drop to database here
-    const url = '/api/drops'
-    await fetch(url, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(drop),
-    })
+    if (message.length > 0) {
+      const url = '/api/drops'
+      await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(drop),
+      })
+    }
+    else {
+      console.log('Add a message')
+    }
 
     // reset form
     setMessage('')
@@ -51,7 +56,7 @@ export const Login = () => {
   if (session) {
     return (
       <>
-        <form id="drops-form" onSubmit={(createDrop)}  className="flex text-sm max-w-md relative">
+        <form id="drops-form" onSubmit={(createDrop)} className="flex text-sm max-w-md relative">
           <input value={message} onChange={(e) => setMessage(e.target.value)} placeholder="Add your drop..." className="bg-stone-100 rounded-md pl-3 pr-20 py-2 w-full"/>
           <button type="submit" className="rounded-md w-16 absolute text-stone-500 hover:text-black transition-all bg-black/[.05] right-1 h-7 top-1 text-xs">Sign</button>
         </form>
